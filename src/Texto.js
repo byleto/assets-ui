036724ec1d0f57c909541115c988407eff241dcc
Byron Torres <byron.torres@telus.com>
2019-07-10 23:51:16 -0500

    fix: [SSC-002] Commit inicial

---
+++ src/Texto.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Texto() {
+  return (
+    <div>
+      <p> type="text" name="hola"</p>
+      <button>click</button>
+    </div>
+  );
+}
+
+export default Texto;
